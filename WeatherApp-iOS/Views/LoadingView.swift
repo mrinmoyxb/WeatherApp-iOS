3ea95265413cdc8f4399a22c3a151529049ffa72
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-04-23 23:06:35 +0530

    location view

---
+++ WeatherApp-iOS/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp-iOS
+//
+//  Created by Mrinmoy Borah on 23/04/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
